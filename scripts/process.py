c815355de84ec1d6aaac6676f008743a925715c2
gradedSystem <homesiknessgerald@gmail.com>
2024-10-07 11:42:25 +0700

    [fix][m] Fixing up the script

--- scripts/process.py
+++ scripts/process.py
@@ -1,27 +1,47 @@
-import urllib
-import zipfile
+import io
+import os
 import csv
+import zipfile
+import requests
 
-version = '19'
-source = "http://www.eea.europa.eu/data-and-maps/data/\
-european-union-emissions-trading-scheme-eu-ets-data-from-citl-7/\
-eu-ets-data-download-latest-version/citl_v"+version+".zip/at_download/file"
-zip = 'tmp/citl.zip'
-csvfile = 'tmp/CITL_v'+version+'.csv'
-out_path = 'data/eu-ets.csv'
+source = "https://sdi.eea.europa.eu/datashare/s/SxpWTfMxkZSrpBo/download"
+zip_file = "data.zip"
 
+def download_zip(url, output_file):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(output_file, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded ZIP file to {output_file}")
+    else:
+        raise Exception(f"Failed to download file: {response.status_code}")
 
-def execute():
-    print('Trying to download from path:')
-    print(source)
-    urllib.urlretrieve(source, zip)
+def process():
+    try:
+        download_zip(source, zip_file)
+        
+        # Open and process the ZIP file
+        with zipfile.ZipFile(zip_file, 'r') as archive:
+            for elem in archive.namelist():
+                if 'CITL' in elem:
+                    file_name = elem
 
-    with zipfile.ZipFile(zip, 'r') as z:
-        z.extractall('tmp/')
+            # Open the file from within the ZIP archive
+            with archive.open(file_name, 'r') as f: 
+                decoded_file = io.TextIOWrapper(f, encoding='utf-8')
+                with open('data/eu-ets.csv', 'w', newline='') as f_out:
+                    fieldnames = ['country_code','country','main activity sector name','ETS information','year','value','unit']
+                    writer = csv.DictWriter(f_out, fieldnames=fieldnames)
+                    writer.writeheader()
+                    # Use DictReader to read the tab-delimited file
+                    for row in csv.DictReader(decoded_file, delimiter='\t'):
+                        writer.writerow(row)
 
-    with open(csvfile, 'r') as infile, open(out_path,'w') as outfile:
-        csvreader = csv.reader(infile, delimiter='\t')
-        csvwriter = csv.writer(outfile)
-        csvwriter.writerows(csvreader)
+        os.remove(zip_file)
+        print("Processing complete, ZIP file removed.")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-execute()
+if __name__=='__main__':
+    process()
